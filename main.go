f1b6f8af958690ff7ee441369659d82de6c5ca36
AdrieneTZ <tznali17@gmail.com>
2023-03-11 11:19:27 +0800

    feat: create an array of sting to store user's first name and last name

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	fmt.Printf("We have total of %v tickets, and %v are still aviliable.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
+	// declare variables
+	var bookings [50]string
+
 	var firstName string
 	var lastName string
 	var email string
@@ -30,6 +33,7 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets are remaining for %v.\n", remainingTickets, conferenceTickets)
